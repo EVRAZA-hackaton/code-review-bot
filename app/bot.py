b29100aaf88302ca0fbf7cbed57dfc317f5889b0
Vitaly Kopylov <workflow.elec@gmail.com>
2024-11-30 14:50:59 +0300

    fix/add new configuration style for bot

--- app/bot.py
+++ app/bot.py
@@ -2,9 +2,11 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
+from aiogram.client.bot import DefaultBotProperties
 
 from app.config.config import Config, load_config
 from app.config.logger import configure_logger
+from app.src.endpoints import echo
 
 logger = logging.getLogger(__name__)
 
@@ -16,8 +18,12 @@ async def _start_bot():
 
     config: Config = load_config()
 
-    bot: Bot = Bot(token=config.tg_bot.token, parse_mode="HTML")
+    bot: Bot = Bot(
+        token=config.tg_bot.token, default=DefaultBotProperties(parse_mode="HTML")
+    )
     dp: Dispatcher = Dispatcher()
+
+    dp.include_router(echo.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
