6c22d9817e7508c3ebaabb897884b2bd98183aab
fvelikon <mirotvorez00@gmail.com>
2024-11-30 16:04:11 +0300

    add prompt classifier for files in a project

--- app/src/model/constants.py
+++ app/src/model/constants.py
@@ -4,3 +4,14 @@ from enum import StrEnum, auto
 class FileExtensionEnum(StrEnum):
     CODE_FILE = auto()
     CONFIGURATION_FILE = auto()
+
+
+class PromptEnum(StrEnum):
+    """Determines which prompy suite to use for a single file"""
+    CODE_PROMPT = auto()
+    CONFIG_PROMPT = auto()
+    PROJECT_STRUCTURE_PROMPT = auto()
+
+    # PYTHON_PROMPT = auto()
+    # JS_PROMPT = auto()
+    # C_SHAPR_PROMPT = auto()
--- app/src/services/classifier.py
+++ app/src/services/classifier.py
@@ -1,9 +1,36 @@
 from app.src.model.project import Project
+from app.src.model.directory import Directory
+from app.src.model.file import File
+from app.src.model.constants import PromptEnum, FileExtensionEnum
 
 
 class ClassifierService:
-    # TODO: опиши модель (датамодель), в котором ты отдашь данные,
-    #  которые примет другой сервис и на основе этих данных
-    #  - подберет правильный промпт для каждого файла
-    async def classify(self, project: Project):
-        ...
+
+    def _parse_dir(
+        self, directory: Directory | Project
+    ) -> list[tuple[File, PromptEnum]]:
+
+        current_level_files: list[File] = directory.files
+
+        current_level_files_enums: list[tuple[File, PromptEnum]] = []
+
+        for file in current_level_files:
+            file_global_extension = file.extension
+
+            match file_global_extension:
+                case FileExtensionEnum.CODE_FILE:
+                    prompt_enum: PromptEnum = PromptEnum.CODE_PROMPT
+                case FileExtensionEnum.CONFIGURATION_FILE:
+                    prompt_enum = PromptEnum.CONFIG_PROMPT
+
+            current_level_files_enums.append((file, prompt_enum))
+
+        for next_level_dir in directory.dirs:
+            current_level_files_enums.extend(self._parse_dir(next_level_dir))
+
+        return current_level_files_enums
+
+    def classify(self, project: Project) -> list[tuple[File, PromptEnum]]:
+        # TODO add project structure prompts
+        # TODO add language-specific prompts
+        return self._parse_dir(project)
